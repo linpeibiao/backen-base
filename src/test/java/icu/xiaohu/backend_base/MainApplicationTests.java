47ce8a040032de511f338ae4657c42788f93453a
沛彪 林 <linpeibiao2022@163.com>
2023-10-05 20:35:59 +0800

    提交测试

--- src/test/java/icu/xiaohu/backend_base/MainApplicationTests.java
+++ src/test/java/icu/xiaohu/backend_base/MainApplicationTests.java
@@ -1,22 +1,17 @@
-package backen_base;
+package icu.xiaohu.backend_base;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author xiaohu
- * @date 2022/07/23/ 15:29
+ * @date 2023/10/05/ 20:34
  * @description
  */
 @SpringBootTest
 @Slf4j
-public class MainApplicationTest {
-    public static void main(String[] args) {
-        SpringApplication.run(icu.xiaohu.backend_base.MainApplication.class, args);
-    }
-
+public class MainApplicationTests {
     @Test
     public void test(){
         log.info("hello world");
